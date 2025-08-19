be2c682106bd2ac0ebc251ef2476ab165f717222
Hossam Ahmed <hossamahmed201515@gmail.com>
2025-08-19 20:43:12 +0300

    config update

--- config/db.js
+++ config/db.js
@@ -2,10 +2,10 @@ const mysql = require('mysql2');
 require('dotenv').config();
 
 const connection = mysql.createConnection({
-  host: process.env.DB_HOST,
-  user: process.env.DB_USER,
-  password: process.env.DB_PASSWORD,
-  database: process.env.DB_NAME
+  host: process.env.NODE_ENV === "development" ? process.env.DB_HOST : process.env.MYSQLHOST,
+  user: process.env.NODE_ENV === "development" ? process.env.DB_USER : process.env.MYSQLUSER,
+  password: process.env.NODE_ENV === "development" ? process.env.DB_PASSWORD : process.env.MYSQLPASSWORD,
+  database: process.env.NODE_ENV === "development" ? process.env.DB_NAME : process.env.MYSQLDATABASE
 });
 
 connection.connect(err => {
