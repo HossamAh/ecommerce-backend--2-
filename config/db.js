1fbfc0bfc6f893a32f2fc6ffe05d6c8a2fc16705
Hossam Ahmed <hossamahmed201515@gmail.com>
2025-08-20 12:36:58 +0300

    update config

--- config/db.js
+++ config/db.js
@@ -1,23 +1,24 @@
 const mysql = require('mysql2');
 require('dotenv').config();
 
-if(process.env.NODE_ENV !== 'development') {
-const connection = mysql.createConnection({
-  host: process.env.DB_HOST,
-  user: process.env.DB_USER,
-  password: process.env.DB_PASSWORD,
-  database: process.env.DB_NAME
-});
-}
-else{
-  const connection = mysql.createConnection(process.env.MYSQL_URL);
-  // const connection = mysql.createConnection({
-  //   host: process.env.MYSQLHOST,
-  //   user: process.env.MYSQLUSER,
-  //   password: process.env.MYSQLPASSWORD,
-  //   database: process.env.MYSQLDATABASE
-  // });
-}
+// if(process.env.NODE_ENV !== 'development') {
+// const connection = mysql.createConnection({
+//   host: process.env.DB_HOST,
+//   user: process.env.DB_USER,
+//   password: process.env.DB_PASSWORD,
+//   database: process.env.DB_NAME
+// });
+// }
+// else{
+//   const connection = mysql.createConnection(process.env.MYSQL_URL);
+//   // const connection = mysql.createConnection({
+//     //   host: process.env.MYSQLHOST,
+//   //   user: process.env.MYSQLUSER,
+//   //   password: process.env.MYSQLPASSWORD,
+//   //   database: process.env.MYSQLDATABASE
+//   // });
+// }
+const connection = mysql.createConnection(process.env.MYSQL_URL);
 
 connection.connect(err => {
   if (err) throw err;
