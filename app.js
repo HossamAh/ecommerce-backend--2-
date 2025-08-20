feec04d35672f3b2206f165aa5c4bd1c545b48cd
Hossam Ahmed <hossamahmed201515@gmail.com>
2025-08-20 12:33:26 +0300

    update app

--- app.js
+++ app.js
@@ -23,6 +23,7 @@ app.use('/api/auth', authRoutes);
 const PORT = process.env.PORT || 3000;
 app.listen(PORT, () => console.log(`Server running on port ${PORT}`));
 
+console.log(process.env.MYSQL_URL);
 const db = require('./models');
 // Only use this in development!
 // In production, use migrations instead
